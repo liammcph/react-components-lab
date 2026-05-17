3c6f8cf19aec323a6883895e206075f2878dbdfd
liammcph <liammcpheeters@gmail.com>
2026-05-17 16:07:08 -0400

    added weather icon and weatherdata

---
+++ src/WeatherData.jsx
@@ -0,0 +1,10 @@
+const WeatherData = (props) => {
+  return (
+    <>
+      <h2>{props.day}</h2>
+      <p><span>conditions: </span>{props.conditions}</p>
+      <p><span>time: </span>{props.time}</p>
+    </>
+  );
+}
+export default WeatherData
---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import './WeatherForecast.css'
+import WeatherIcon from './WeatherIcon'
+import WeatherData from './WeatherData'
+
+
+const WeatherForecast = (props) => {
+  return (
+    <div className="weather">
+      <WeatherIcon img={props.img} imgAlt={props.imgAlt} />
+      <WeatherData day={props.day} conditions={props.conditions} time={props.time} />
+    </div>
+  );
+}
+export default WeatherForecast
